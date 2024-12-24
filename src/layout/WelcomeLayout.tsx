3bd1f929bb24aa1edcf2a353c5de34ca4296dfb6
AndresElacion <andreselacioniii@gmail.com>
2024-12-24 15:35:48 +0800

    fix nav

--- src/layout/WelcomeLayout.tsx
+++ src/layout/WelcomeLayout.tsx
@@ -7,7 +7,7 @@ export default function WelcomeLayout() {
       <div className="fixed top-0 left-0 w-full bg-white shadow-md z-10">
         <div className="flex container max-w-7xl mx-auto items-center justify-between p-4">
           <h1 className="text-lg font-bold">cionui</h1>
-          <nav className="space-x-3 hidden lg:flex">
+          <nav className="space-x-3">
             <Link to="/" className="hover:text-blue-600">
               Home
             </Link>
