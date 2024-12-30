4c00d1097efe5d256a244bdf88787e76dd8ba9a1
AndresElacion <andreselacioniii@gmail.com>
2024-12-30 18:37:20 +0800

    added design1 for hero

---
+++ src/pages/Hero.tsx
@@ -0,0 +1,31 @@
+import { useOutletContext } from "react-router-dom";
+import HeroDesign1 from "../components/hero/HeroDesign1";
+
+interface OutletContext {
+  sidebarOpen: boolean;
+}
+
+export default function Hero() {
+  // Use the context from the Layout component
+  const { sidebarOpen } = useOutletContext<OutletContext>();
+
+  return (
+    <div
+      className={`my-7 transition-all ${
+        sidebarOpen ? "ml-18" : "ml-0"
+      } lg:ml-18`}
+    >
+      <div className="max-w-7xl mx-auto flex items-center space-x-2 text-sm text-gray-500 px-4">
+        <a href="/components" className="text-blue-500 hover:underline">
+          Components
+        </a>
+        <span>/</span>
+        <h1>Hero</h1>
+      </div>
+
+      <div className="mb-12 px-4">
+        <HeroDesign1 />
+      </div>
+    </div>
+  );
+}
