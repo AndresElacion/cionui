dfde522c690a2418247376144f4235f26512bebf
AndresElacion <andreselacioniii@gmail.com>
2025-01-14 23:34:55 +0800

    added order

--- src/backend/supabaseClient.tsx
+++ src/backend/supabaseClient.tsx
@@ -1,5 +1,5 @@
-import { createClient } from '@supabase/supabase-js'
-import { Database } from '../../supabase-types'
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "../../supabase-types";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
@@ -7,12 +7,13 @@ const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
 
 export const getPaginatedFeedback = async (page: number, pageSize: number) => {
-    const { data, error, count } = await supabase
-        .from('feedback')
-        .select('*', { count: 'exact' })
-        .range((page - 1) * pageSize, page * pageSize - 1)
+  const { data, error, count } = await supabase
+    .from("feedback")
+    .select("*", { count: "exact" })
+    .order("created_at", { ascending: false })
+    .range((page - 1) * pageSize, page * pageSize - 1);
 
-    if (error) throw error
+  if (error) throw error;
 
-    return { data, count }
-}
+  return { data, count };
+};
