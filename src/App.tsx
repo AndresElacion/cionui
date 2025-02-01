ecea07df8fe4a411cfea71bf146d6261a2a48148
Andres Elacion III <andreselacioniii@gmail.com>
2025-02-01 19:12:51 +0800

    added routes for axios components for laravel and react

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import CTA from "./pages/CTA";
 import FeedbackData from "./pages/FeedbackData";
 import FeedbackLayout from "./layout/FeedbackLayout";
 import Nav from "./pages/Nav";
+import AxiosComponent from "./components/ReactLaravel/AxiosComponent";
 
 const NotFound = () => (
   <h1 className="text-center mt-10">404 - Page Not Found</h1>
@@ -36,6 +37,7 @@ const App: React.FC = () => {
           <Route path="Hero" element={<Hero />} />
           <Route path="Cta" element={<CTA />} />
           <Route path="Nav" element={<Nav />} />
+          <Route path="ReactLaravel" element={<AxiosComponent />} />
         </Route>
         
         <Route element={<FeedbackLayout />}>
